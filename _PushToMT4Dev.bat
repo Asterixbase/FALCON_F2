99248bbdd3602a0ffd8b84dc3523f39f733f88bc
Asterixbase <jeffrey102000@yahoo.com>
2021-02-25 22:47:44 +0000

    try

--- _PushToMT4Dev.bat
+++ _PushToMT4Dev.bat
@@ -7,7 +7,7 @@ setlocal enabledelayedexpansion
 :: Source Directory where Version Control Repository is located
 set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\FALCON_F2"
 :: Destination Directory where Expert Advisor is located
-set DEST_DIR="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\FALCON_F2"
+set DEST_DIR="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\02_FALCON_F2"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mq4
 
